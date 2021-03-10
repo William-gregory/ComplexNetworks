cc96aa12b591c840e42783a93ec36c573e9189ed
Will Gregory <William.gregory@outlook.com>
2021-03-10 19:27:50 +0000

    Update CN.py

--- CN.py
+++ CN.py
@@ -26,7 +26,7 @@ class Network:
         self.strength = strength
         self.strengthmap = strengthmap
     
-    def tau(self, significance=0.01):
+    def threshold(self, significance=0.01):
         """
         Compute pairwise correlations between all grid cells.
         The average of all correlations which are positive and
@@ -51,7 +51,7 @@ class Network:
 
         self.tau = np.mean(R)
     
-    def area_level(self, latlon_grid=False):
+    def vertices(self, latlon_grid=False):
         ids = np.where(np.isnan(self.data[:,:,:]))
         i_nan = ids[0][0] ; j_nan = ids[1][0]
 
@@ -244,7 +244,7 @@ class Network:
                         self.unavail.append(self.V[max_ID][i])
                       
                 
-    def intra_links(self, area=None, lat=None):
+    def edges(self, area=None, lat=None):
         print('Generating network links')
         self.anomaly = {}
         self.links = {}
